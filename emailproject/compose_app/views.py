82ec07396799fe74b9107b86127878989ed7e9ba
Agam Dua <agam.dua@contentflow.in>
2013-06-23 14:33:33 +0530

    Added form for compose and view where compose is shown. Form is_valid() is called and if not then unbounded form is returned. Planning to change this to return a bounded form with correct data in place rather than a completely blank (unbounded) form.

--- emailproject/compose_app/views.py
+++ emailproject/compose_app/views.py
@@ -1,1 +1,25 @@
 # Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.mail import send_mail
+
+def compose(request):
+    if request.method == 'POST':
+        form = ComposeMessage(request.POST)
+
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            mail_content = form.cleaned_data['mail_content']
+            recipients.append(to)
+            # recipients.append(cc)
+            # recipients.append(bcc)
+            sender = agamdua@gmail.com # Hard coding user email
+                
+            send_mail(subject, mail_content, sender, recipients)
+            return HttpResponseRedirect('/thanks/') # Redirect after post
+
+        else:
+            form = ComposeMessage() # An unbound form
+        
+        return
+
