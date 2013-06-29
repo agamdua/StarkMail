7d4840fe5333f75eec5633d412ca52c31f8241d3
Agam Dua <agam.dua@contentflow.in>
2013-06-29 12:40:32 +0530

    Redirect after registration problem fixed.

--- emailproject/register_app/views.py
+++ emailproject/register_app/views.py
@@ -4,19 +4,20 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from forms import UserCreateForm
 from django.template import RequestContext
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login
 
 def post(request):
 
-    user_form = UserCreateForm(request.POST)
+    if request.method == 'POST':
+        user_form = UserCreateForm(request.POST)
     
-    if user_form.is_valid():
-        username = user_form.clean_username()
-        password = user_form.clean_password2()
-        user_form.save()
-        user = authenticate(username=username, password=password)
-        login(request, user)
-        return HttpResponseRedirect('/compose/')
+        if user_form.is_valid():
+            username = user_form.clean_username()
+            password = user_form.clean_password2()
+            user_form.save()
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return HttpResponseRedirect('/compose/')
 
     else:
         user_form = UserCreateForm()
