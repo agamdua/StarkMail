15a69caf38f5bb322db22a528a086e92ef35d23e
Agam Dua <agam.dua@contentflow.in>
2013-07-04 01:02:57 +0530

    Brought the compose_app to the Mailgun API. Tested successfully

--- emailproject/compose_app/views.py
+++ emailproject/compose_app/views.py
@@ -1,13 +1,21 @@
 # Create your views here.
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from django.core.mail import send_mail
+# from django.core.mail import send_mail
 from django.contrib.auth.decorators import login_required
-from forms import ComposeForm
+from django.contrib.auth.models import User
+
+
+from .forms import ComposeForm
+# from .send_mail import send_simple_message
+from untracked import auth, api_url
+
+import requests
+
 
 @login_required()
 def compose(request):
-    
+    username = request.user.username
     recipients = []
 
     if request.method == 'POST':
@@ -20,11 +28,19 @@ def compose(request):
             recipients.append(to)
             # recipients.append(cc)
             # recipients.append(bcc)
-            sender = 'agamdua@gmail.com' # Hard coding user email
-
+            sender = username + "@starkmail.mailgun.org"
             form.save()
-                
-            send_mail(subject, mail_text, sender, recipients)
+
+            # send_mail(subject, mail_text, sender, recipients)
+            requests.post(
+                url=api_url,
+                auth=auth,
+                data = {"from" : sender,
+                        "to" : recipients,
+                        "subject" : subject,
+                        "text" : mail_text },
+                verify=False)
+
             return HttpResponseRedirect('/compose/') # Redirect after post
 
     else:
