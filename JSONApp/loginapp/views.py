79e32ee8ad28acfaaaf82ed226adf8aeafb9a6b2
Parth <parth@The-Time-Machine.(none)>
2013-07-06 15:06:02 +0530

    views for login app

--- JSONApp/loginapp/views.py
+++ JSONApp/loginapp/views.py
@@ -1,1 +1,27 @@
 # Create your views here.
+
+def login_view():
+	if request.method=='POST':
+			login_form=UserLoginForm(request=request, data=request.POST)
+
+			username=resquest.POST.get('username' , '')
+			password=resquest.POST.get('password' , '')
+
+
+        	user = authenticate(username=username, password=password)
+
+        	if login_form.is_valid:
+            	login(request, user)
+            	return HttpResponseRedirect('/home/')
+            
+        # else:
+        # return HttpResponseRedirect('/')
+
+
+    else:
+        login_form = UserLoginForm()
+
+
+    context_login = { 'login_form' : login_form }
+
+    return TemplateResponse(request, 'login_form.html', context_login)
