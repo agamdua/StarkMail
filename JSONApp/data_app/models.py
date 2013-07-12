711708bf22d765b4207321e7f7c38239edcbb5b3
Parth <parth@The-Time-Machine.(none)>
2013-07-12 18:08:09 +0530

    models.py updated with content data

--- JSONApp/data_app/models.py
+++ JSONApp/data_app/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class CourseID(models.Model):
+	course_id=models.CharField(max_length=20, primary_key=True)
+	course_name=models.CharField(max_length=100)
+
+class SubjectID(models.Model):
+	subject_id=models.CharField(max_length=20, primary_key=True)
+	c_id=models.ForeignKey(CourseID)
+	subject_name=models.CharField(max_length=100)
+
+class TopicID(models.Model):
+	topic_id=models.CharField(max_length=20, primary_key=True)
+	c_id=models.ForeignKey(CourseID)
+	s_id=models.ForeignKey(SubjectID)
+	topic_name=models.CharField(max_length=100)
+	topic_description=models.CharField(max_length=1000)
+
+class SubtopicID(models.Model):
+	subtopic_id=models.CharField(max_length=20, primary_key=True)
+	c_id=models.ForeignKey(CourseID)
+	s_id=models.ForeignKey(SubjectID)
+	t_id=models.ForeignKey(TopicID)
+	subtopic_name=models.CharField(max_length=200)
+
+
+
+
+
+
